8ebfe551455309528cc62afa8b5167f45f385ef8
John Starks <jostarks@microsoft.com>
2024-11-08 20:51:51 +0000

    regen

--- openhcl/underhill_core/src/partition.rs
+++ openhcl/underhill_core/src/partition.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 //! Partition abstraction allowing different virtualization backends within
 //! OpenHCL.
 //!
--- openhcl/underhill_mem/src/devmem.rs
+++ openhcl/underhill_mem/src/devmem.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 // UNSAFETY: required for mapping memory to a partition.
 #![allow(unsafe_code)]
 
